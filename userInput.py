98e982b8b6d558407f85e80107049d2f23234916
Saksham Garg <gargsaksham1895@gmail.com>
2026-01-30 01:27:20 +0530

    User Input exercises completed

--- userInput.py
+++ userInput.py
@@ -12,3 +12,20 @@ age = age + 1
 
 print("Your name is: ", name)
 print("Your age is: ", age)
+
+
+# exercise 1, Calculate Area calc
+length = float(input("Enter Length: "))
+width = float(input("Enter Width: "))
+
+area = length * width
+
+print(area)
+
+# Exercise 2 Shopping cart Program
+item = input("What item you want to buy: ")
+price = int(input("Enter Price: "))
+quantity = int(input("Enter Quantity: "))
+total = price * quantity
+
+print("your total cart is: ", total)
