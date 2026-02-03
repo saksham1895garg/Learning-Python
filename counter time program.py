236dc7cfe3afa339aaa7d6f21c457f6d88d59999
Saksham Garg <gargsaksham1895@gmail.com>
2026-02-04 03:00:09 +0530

    counter timer program completed with for loops

---
+++ counter time program.py
@@ -0,0 +1,10 @@
+import time
+
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range ( my_time, 0, -1):
+    print(x)
+    time.sleep(1)
+
+print("times up")
---
+++ for loops.py
@@ -0,0 +1,10 @@
+#for loops = execute a block of code a fixed number of them
+             # you can iterate over a range, string, sequence etc
+
+
+
+
+for x in reversed(range(1, 11, 2)):
+    print(x)
+
+
